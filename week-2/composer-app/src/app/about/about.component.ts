42074d2d208f83f13445a4462b86001a40daace2
Jeff Lintel <jefflintel@gmail.com>
2020-07-19 17:24:05 -0500

    week 2 initial commit

--- week-2/composer-app/src/app/about/about.component.ts
+++ week-2/composer-app/src/app/about/about.component.ts
@@ -1,3 +1,11 @@
+/* ============================================
+ ; Title:  about.component.ts
+ ; Author: Professor Krasso
+ ; Date:   19 July 2020
+ ; Modified by: Jeff Lintel
+ ; Description: about component
+ ===========================================*/
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
