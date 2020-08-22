471eb7859eab3cc62c1d445c89fcfd51e00fe559
Jeff Lintel <jefflintel@gmail.com>
2020-08-21 21:46:28 -0500

    added 7.2

---
+++ week-7/gpa-calculator-app3/src/app/home/home.component.ts
@@ -0,0 +1,90 @@
+/* ============================================
+ ; Title:  home.component.ts
+ ; Author: Professor Krasso
+ ; Date:   14 August 2020
+ ; Modified by: Jeff Lintel
+ ; Description: home component
+ ===========================================*/
+
+import { Component, OnInit } from '@angular/core';
+
+import { ITranscript } from '../transcript.interface';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  transcriptEntry: ITranscript;
+  selectableGrades: Array<string> = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F'];
+  transcriptEntries: Array<ITranscript> = [];
+  gpaTotal: number = 0;
+
+  constructor() {
+    this.transcriptEntry = {} as ITranscript;
+  }
+
+  ngOnInit(): void {
+  }
+
+  saveEntry() {
+    this.transcriptEntries.push(this.transcriptEntry);
+    this.transcriptEntry = {} as ITranscript;
+  }
+
+  calculateResults() {
+    let gpa: number = 0;
+    for(let entry of this.transcriptEntries) {
+      console.log(entry.grade)
+      switch(entry.grade) {
+        case 'A':
+          gpa += 4.00;
+          break;
+        case 'A-':
+          gpa += 3.70;
+          break;
+        case 'B+':
+          gpa += 3.33; //note this is different than C+ or D+
+          break;
+        case 'B':
+          gpa += 3.00;
+          break;
+        case 'B-':
+          gpa += 2.70;
+          break;
+        case 'C+':
+          gpa += 2.30;
+          break;
+        case 'C':
+          gpa += 2.00;
+          break;
+        case 'C-':
+          gpa += 1.70;
+          break;
+        case 'D+':
+          gpa += 1.30;
+          break;
+        case 'D':
+          gpa += 1.00;
+          break;
+        case 'D-':
+          gpa += 0.70;
+          break;
+        default:
+          gpa += 0.00;
+          break;
+      }
+    }
+
+    this.gpaTotal = gpa / this.transcriptEntries.length;
+
+  }
+
+  clearEntries() {
+    this.transcriptEntries = [];
+    this.gpaTotal = 0;
+  }
+
+}
